7cbc7c2049a0cb76acf7b7a265aa9d14f75ff891
Katerina881 <mr.alodov@mail.ru>
2022-04-04 12:48:39 +0400

    merge 2

---
+++ ComputersShop/ComputersShopContracts/ViewModels/ReportWareHouseComponentViewModelFlyoutMenuItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputersShopContracts.ViewModels
+{
+    public class ReportWareHouseComponentViewModelFlyoutMenuItem
+    {
+        public ReportWareHouseComponentViewModelFlyoutMenuItem()
+        {
+            TargetType = typeof(ReportWareHouseComponentViewModelFlyoutMenuItem);
+        }
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public Type TargetType { get; set; }
+    }
+}
